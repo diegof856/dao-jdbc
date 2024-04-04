673f55c4482233df8d8c0eb6d58ff86cfa4c21e3
Diego Felipe <diegofelipe1025@gmail.com>
2024-04-04 12:07:44 -0300

    implementacao do metodo delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,7 +11,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		 
+		 Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
@@ -38,6 +39,13 @@ public class Program {
 		seller.setName("Maria");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("=== TEST 6: seller delete ===");
+		System.out.print("Id: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		sc.close();
 	}
 
 }
